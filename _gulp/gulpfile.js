2a88f44b64e37d8e2d99599296cf4dcd29b455d3
kei-nishio <kei.nishio.031107@gmail.com>
2024-05-11 17:43:58 +0900

    gulp php col

--- _gulp/gulpfile.js
+++ _gulp/gulpfile.js
@@ -9,8 +9,8 @@ import through2 from 'through2'; // gulpの処理を通す
 import rename from 'gulp-rename'; // ファイル名変更
 import browserSync from 'browser-sync'; // ブラウザの自動リロード
 // ** CSS/Sass処理
-import gulpSassCreator from "gulp-sass";
-import * as sassImplementation from "sass";
+import gulpSassCreator from 'gulp-sass';
+import * as sassImplementation from 'sass';
 import sassGlob from 'gulp-sass-glob-use-forward'; // SCSSのインポートを簡略化
 import mmq from 'gulp-merge-media-queries'; // メディアクエリをマージ
 import postcss from 'gulp-postcss'; // CSS変換処理
@@ -32,32 +32,32 @@ import os from 'os'; // OSモジュール
 
 // ** その他の設定
 const sass = gulpSassCreator(sassImplementation); // SCSSをCSSにコンパイルするためのモジュール
-const browsers = ["last 2 versions", "> 5%", "ie = 11", "not ie <= 10", "ios >= 8", "and_chr >= 5", "Android >= 5"];
+const browsers = ['last 2 versions', '> 5%', 'ie = 11', 'not ie <= 10', 'ios >= 8', 'and_chr >= 5', 'Android >= 5'];
 const userHomeDir = os.homedir(); // ホームディレクトリを取得：C:\Users\userName
 
 // ** パス設定
 const wpMode = false; // ! WordPressの場合はtrueにする（静的コーディングのみの場合はfalse）
-const siteTitle = "hogeTemplate"; // ! WordPress site title (project name)
-const themeName = "hogeTemplateTheme"; // ! WordPress theme file name
-const localSiteDomain = "hogeTemplate.local"; // ! WordPress Local Site Domain
+const siteTitle = 'hogeTemplate'; // ! WordPress site title (project name)
+const themeName = 'hogeTemplateTheme'; // ! WordPress theme file name
+const localSiteDomain = 'hogeTemplate.local'; // ! WordPress Local Site Domain
 const wpDirectory = `${userHomeDir}/Local Sites/${siteTitle}/app/public/wp-content/themes/${themeName}`;
 
 // 読み込み先
 const srcPath = {
-  css: "../src/sass/**/*.scss",
-  js: "../src/assets/js/**/*",
-  img: "../src/assets/images/**/*",
-  html: ["../src/**/*.html", "!./node_modules/**"],
-  php: ["../src/wp/**/*.php", "../src/wp/style.css", "../src/wp/screenshot.*"],
+  css: '../src/sass/**/*.scss',
+  js: '../src/assets/js/**/*',
+  img: '../src/assets/images/**/*',
+  html: ['../src/**/*.html', '!./node_modules/**'],
+  php: ['../src/wp/**/*.php', '../src/wp/style.css', '../src/wp/screenshot.*'],
 };
 
 // html反映用
 const destPath = {
-  all: "../dist/**/*",
-  css: "../dist/assets/css/",
-  js: "../dist/assets/js/",
-  img: "../dist/assets/images/",
-  html: "../dist/",
+  all: '../dist/**/*',
+  css: '../dist/assets/css/',
+  js: '../dist/assets/js/',
+  img: '../dist/assets/images/',
+  html: '../dist/',
 };
 
 // WordPress反映用
@@ -86,85 +86,79 @@ const htmlCopy = () => {
 // * PHPファイルのコピー
 const phpCopy = () => {
   if (wpMode) {
-    return (
-      src(srcPath.php)
-        .pipe(dest(destWpPath.php)) // WordPress反映用
-        .pipe(dest(destWpLocalPath.php)) // WordPressLocal反映用
-    );
+    return src(srcPath.php)
+      .pipe(dest(destWpPath.php)) // WordPress反映用
+      .pipe(dest(destWpLocalPath.php)); // WordPressLocal反映用
   } else {
-    return src(".").pipe(dest(".")); // falseの場合は何も実行せず、undefinedを返す
+    return Promise.resolve(); // falseの場合は何も実行せず、Promiseを返す
   }
 };
 
 // * SASSファイルのコンパイル
 const cssSass = () => {
-  return (
-    src(srcPath.css)
-      .pipe(sourcemaps.init()) // ソースマップを初期化
-      .pipe(plumber({ errorHandler: notify.onError("Error:<%= error.message %>"), })) // エラーが発生してもタスクを続行
-      .pipe(sassGlob()) // Sassのパーシャル（_ファイル）を自動的にインポート
-      .pipe(sass.sync({ includePaths: ["src/sass"], outputStyle: "expanded", })) // コンパイル後のCSSの書式（expanded or compressed）
-      .pipe(postcss([
+  return src(srcPath.css)
+    .pipe(sourcemaps.init()) // ソースマップを初期化
+    .pipe(plumber({ errorHandler: notify.onError('Error:<%= error.message %>') })) // エラーが発生してもタスクを続行
+    .pipe(sassGlob()) // Sassのパーシャル（_ファイル）を自動的にインポート
+    .pipe(sass.sync({ includePaths: ['src/sass'], outputStyle: 'expanded' })) // コンパイル後のCSSの書式（expanded or compressed）
+    .pipe(
+      postcss([
         postcssPresetEnv({ browsers: 'last 2 versions' }), // 未来のCSS構文を使用可能にし、対象ブラウザを最新2バージョンに限定
         autoprefixer({ grid: true }), // ベンダープレフィックスを自動で付与、グリッドレイアウトをサポート
-        cssdeclsort({ order: 'alphabetical' }) // CSSプロパティをアルファベット順にソート
-      ]))
-      .pipe(mmq())
-      .pipe(sourcemaps.write('./')) // ソースマップをここで書き出す
-      .pipe(dest(destPath.css)) // 非圧縮CSSを出力
-      .pipe(wpMode ? dest(destWpPath.css) : through2.obj())
-      .pipe(wpMode ? dest(destWpLocalPath.css) : through2.obj())
-      .pipe(rename({ suffix: '.min' })) // ファイル名に.minを追加
-      .pipe(cleanCSS()) // CSSを圧縮
-      .pipe(dest(destPath.css)) // 圧縮CSSを出力
-      .pipe(wpMode ? dest(destWpPath.css) : through2.obj())
-      .pipe(wpMode ? dest(destWpLocalPath.css) : through2.obj())
-      .pipe(notify({ message: "Sassをコンパイルしました！", onLast: true, })) // 通知を表示
-  );
+        cssdeclsort({ order: 'alphabetical' }), // CSSプロパティをアルファベット順にソート
+      ])
+    )
+    .pipe(mmq())
+    .pipe(sourcemaps.write('./')) // ソースマップをここで書き出す
+    .pipe(dest(destPath.css)) // 非圧縮CSSを出力
+    .pipe(wpMode ? dest(destWpPath.css) : through2.obj())
+    .pipe(wpMode ? dest(destWpLocalPath.css) : through2.obj())
+    .pipe(rename({ suffix: '.min' })) // ファイル名に.minを追加
+    .pipe(cleanCSS()) // CSSを圧縮
+    .pipe(dest(destPath.css)) // 圧縮CSSを出力
+    .pipe(wpMode ? dest(destWpPath.css) : through2.obj())
+    .pipe(wpMode ? dest(destWpLocalPath.css) : through2.obj())
+    .pipe(notify({ message: 'Sassをコンパイルしました！', onLast: true })); // 通知を表示
 };
 
 // * 画像圧縮
 const imgImagemin = () => {
-  return (
-    src(srcPath.img,{encoding: false})
-      .pipe(changed(destPath.img)) // 画像の変更を監視
-      .pipe(
-        imagemin(
-          [
-            imageminMozjpeg({ quality: 80, }), // JPEG圧縮品質（0〜100）
-            imageminPngquant(), // PNG圧縮品質（0〜1）
-            imageminSvgo({ plugins: [{ removeViewbox: false, },], }), // SVG画像　viewBox属性を削除しない
-          ],
-          {
-            verbose: true, // 圧縮情報を表示
-          }
-        )
+  return src(srcPath.img, { encoding: false })
+    .pipe(changed(destPath.img)) // 画像の変更を監視
+    .pipe(
+      imagemin(
+        [
+          imageminMozjpeg({ quality: 80 }), // JPEG圧縮品質（0〜100）
+          imageminPngquant(), // PNG圧縮品質（0〜1）
+          imageminSvgo({ plugins: [{ removeViewbox: false }] }), // SVG画像　viewBox属性を削除しない
+        ],
+        {
+          verbose: true, // 圧縮情報を表示
+        }
       )
-      .pipe(dest(destPath.img))
-      .pipe(wpMode ? dest(destWpPath.img) : through2.obj())
-      .pipe(wpMode ? dest(destWpLocalPath.img) : through2.obj())
-      .pipe(webp()) //webpに変換
-      .pipe(dest(destPath.img))
-      .pipe(wpMode ? dest(destWpPath.img) : through2.obj())
-      .pipe(wpMode ? dest(destWpLocalPath.img) : through2.obj())
-  );
+    )
+    .pipe(dest(destPath.img))
+    .pipe(wpMode ? dest(destWpPath.img) : through2.obj())
+    .pipe(wpMode ? dest(destWpLocalPath.img) : through2.obj())
+    .pipe(webp()) //webpに変換
+    .pipe(dest(destPath.img))
+    .pipe(wpMode ? dest(destWpPath.img) : through2.obj())
+    .pipe(wpMode ? dest(destWpLocalPath.img) : through2.obj());
 };
 
 // * js圧縮
 const jsBabel = () => {
-  return (
-    src(srcPath.js)
-      .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>"), })) // エラーが発生してもタスクを続行
-      .pipe(babel({ presets: ["@babel/preset-env"], })) // ES6+のJavaScriptをES5に変換
-      .pipe(dest(destPath.js))
-      .pipe(wpMode ? dest(destWpPath.js) : through2.obj())
-      .pipe(wpMode ? dest(destWpLocalPath.js) : through2.obj())
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(uglify()) //js圧縮
-      .pipe(dest(destPath.js))
-      .pipe(wpMode ? dest(destWpPath.js) : through2.obj())
-      .pipe(wpMode ? dest(destWpLocalPath.js) : through2.obj())
-  );
+  return src(srcPath.js)
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })) // エラーが発生してもタスクを続行
+    .pipe(babel({ presets: ['@babel/preset-env'] })) // ES6+のJavaScriptをES5に変換
+    .pipe(dest(destPath.js))
+    .pipe(wpMode ? dest(destWpPath.js) : through2.obj())
+    .pipe(wpMode ? dest(destWpLocalPath.js) : through2.obj())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify()) //js圧縮
+    .pipe(dest(destPath.js))
+    .pipe(wpMode ? dest(destWpPath.js) : through2.obj())
+    .pipe(wpMode ? dest(destWpLocalPath.js) : through2.obj());
 };
 
 // * ブラウザシンクの設定
@@ -174,7 +168,7 @@ const browserSyncOption = {
 if (wpMode) {
   browserSyncOption.proxy = `http://${localSiteDomain}/`; // WordPressLocal反映用
 } else {
-  browserSyncOption.server = "../dist/"; // 静的コーディング反映用
+  browserSyncOption.server = '../dist/'; // 静的コーディング反映用
 }
 const browserSyncFunc = () => {
   browserSync.init(browserSyncOption);
@@ -201,11 +195,7 @@ const watchFiles = () => {
 };
 
 // ! ブラウザシンク付きの開発用タスク
-export default series(
-  series(cssSass, jsBabel, imgImagemin, htmlCopy, phpCopy),
-  parallel(watchFiles, browserSyncFunc)
-);
+export default series(series(cssSass, jsBabel, imgImagemin, htmlCopy, phpCopy), parallel(watchFiles, browserSyncFunc));
 
 // ! 本番用タスク
 export const build = series(clean, cssSass, jsBabel, imgImagemin, htmlCopy, phpCopy);
-
