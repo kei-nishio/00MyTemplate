9d0ed6ed6f025723ba6ef16ae3911db771221d88
kei-nishio <kei.nishio.031107@gmail.com>
2024-09-07 09:00:07 +0900

    修正

--- src/wp/functions.php
+++ src/wp/functions.php
@@ -57,3 +57,9 @@ get_template_part('/parts/functions-lib/fncs_scf_field_validation');
 
 // ! 言語によって読み込むテンプレートをカスタムする
 get_template_part('/parts/functions-lib/fncs_template-customize');
+
+// ! reCAPTCHAを問い合わせだけに表示させる
+add_action('wp_enqueue_scripts', function () {
+  if (is_page(array('contact', 'confirm', 'thanks'))) return;
+  wp_deregister_script('google-recaptcha');
+}, 100, 0);
