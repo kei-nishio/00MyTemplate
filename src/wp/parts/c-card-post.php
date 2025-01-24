f3e3ab9db9a42f0dc661cbd60e8581b6b4b30f2e
kei-nishio <kei.nishio.0311107@gmail.com>
2025-01-24 12:13:15 +0900

    card-post修正

--- src/wp/parts/c-card-post.php
+++ src/wp/parts/c-card-post.php
@@ -38,58 +38,60 @@ $description = nl2br(esc_html(get_the_author_meta('description')));
 // endforeach;
 ?>
 <a href="<?php the_permalink(); ?>" class="<?php echo $class; ?>">
-  <?php // eye catch 
+  <?php if (false): // eye catch 
   ?>
-  <figure class="<?php echo $class; ?>__img">
-    <?php if (has_post_thumbnail()) : ?>
-      <img src="<?php the_post_thumbnail_url("full"); ?>" alt="<?php echo esc_attr($thumbnail_alt); ?>">
-    <?php else : ?>
-      <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/common/noimage.jpg")); ?>" alt="no image" />
-    <?php endif; ?>
-  </figure>
-  <?php // title 
-  ?>
-  <h2 class="<?php echo $class; ?>__title"><?php echo esc_html(mb_strimwidth(get_the_title(), 0, 18 * 2 + 3, '...')); ?></h2>
-  <?php  // date 
-  ?>
-  <time class="<?php echo $class; ?>__date" datetime="<?php the_time("c"); ?>">
-    <?php the_time("Y.m.d"); ?>
-  </time>
-  <?php // taxonomy 
-  ?>
-  <?php if (!empty($terms) && !is_wp_error($terms)) : ?>
-    <ul class="<?php echo $class; ?>__term-list">
-      <?php if (true): ?>
-        <?php // 複数のタームを出力する 
-        ?>
-        <?php if (true): ?>
-          <?php // 全てのタームを出力する 
-          ?>
-          <?php foreach ($terms as $term) : ?>
-            <li class="<?php echo $class; ?>__term-item">
-              <p class="<?php echo $class; ?>__term"><?php echo  esc_html($term->name); ?></p>
-            </li>
-          <?php endforeach; ?>
-        <?php else: ?>
-          <?php // n個のタームを出力する 
-          ?>
-          <?php for ($i = 0; $i < 1; $i++): ?>
-            <li class="<?php echo $class; ?>__term-item">
-              <p class="<?php echo $class; ?>__term"><?php echo  esc_html($term[$i]->name); ?></p>
-            </li>
-          <?php endfor; ?>
-        <?php endif; ?>
-      <?php else: ?>
-        <?php // 一番目のタームを出力する 
-        ?>
-        <p class="<?php echo $class; ?>__term"><?php echo  esc_html($term[0]->name); ?></p>
+    <figure class="<?php echo $class; ?>__img">
+      <?php if (has_post_thumbnail()) : ?>
+        <img src="<?php the_post_thumbnail_url("full"); ?>" alt="<?php echo esc_attr($thumbnail_alt); ?>">
+      <?php else : ?>
+        <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/common/noimage.jpg")); ?>" alt="no image" />
       <?php endif; ?>
-    </ul>
+    </figure>
   <?php endif; ?>
-  <?php // excerpt 
+  <?php if (true): // title 
   ?>
-  <p class="<?php echo $class; ?>__excerpt"><?php echo esc_html(get_the_excerpt()); ?></p>
-  <?php // content 
+    <h2 class="<?php echo $class; ?>__title"><?php echo esc_html(mb_strimwidth(get_the_title(), 0, 18 * 2 + 3, '...')); ?></h2>
+  <?php endif; ?>
+  <?php if (true): // date 
   ?>
-  <p class="<?php echo $class; ?>__content"><?php echo esc_html(mb_strimwidth(strip_tags(get_the_content()), 0, 100 * 2 + 3, '...')); ?></p>
+    <time class="<?php echo $class; ?>__date" datetime="<?php the_time("c"); ?>">
+      <?php the_time("Y.m.d"); ?>
+    </time>
+  <?php endif; ?>
+  <?php if (true): // taxonomy 
+  ?>
+    <?php if (!empty($terms) && !is_wp_error($terms)) : ?>
+      <ul class="<?php echo $class; ?>__term-list">
+        <?php if (true): // 複数のタームを出力する 
+        ?>
+          <?php if (true): // 全てのタームを出力する 
+          ?>
+            <?php foreach ($terms as $term) : ?>
+              <li class="<?php echo $class; ?>__term-item">
+                <p class="<?php echo $class; ?>__term"><?php echo  esc_html($term->name); ?></p>
+              </li>
+            <?php endforeach; ?>
+          <?php else:  // n個のタームを出力する 
+          ?>
+            <?php for ($i = 0; $i < 1; $i++): ?>
+              <li class="<?php echo $class; ?>__term-item">
+                <p class="<?php echo $class; ?>__term"><?php echo  esc_html($term[$i]->name); ?></p>
+              </li>
+            <?php endfor; ?>
+          <?php endif; ?>
+        <?php else: // 一番目のタームを出力する  
+        ?>
+          <p class="<?php echo $class; ?>__term"><?php echo  esc_html($term[0]->name); ?></p>
+        <?php endif; ?>
+      </ul>
+    <?php endif; ?>
+  <?php endif; ?>
+  <?php if (true): // excerpt 
+  ?>
+    <p class="<?php echo $class; ?>__excerpt"><?php echo esc_html(get_the_excerpt()); ?></p>
+  <?php endif; ?>
+  <?php if (true): // content 
+  ?>
+    <p class="<?php echo $class; ?>__content"><?php echo esc_html(mb_strimwidth(strip_tags(get_the_content()), 0, 100 * 2 + 3, '...')); ?></p>
+  <?php endif; ?>
 </a>
