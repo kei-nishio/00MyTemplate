dee6c8942b1e0ccdac5c398986e9488402bdaff7
kei-nishio <kei.nishio.0311107@gmail.com>
2025-03-07 15:12:50 +0900

    スクリプトの読み込み順序を変更し、依存関係を整理しました

--- src/wp/functions-lib/f-0base_script.php
+++ src/wp/functions-lib/f-0base_script.php
@@ -44,10 +44,10 @@ function my_script_init()
   wp_enqueue_style('style-main', get_theme_file_uri('/assets/css/style.css'), ['style-reset', 'style-swiper'], '1.0.0');
 
   // ! Script
-  wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', "", "11.0", true);
-  wp_enqueue_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js', ['swiper'], "3.12.5", true);
-  wp_enqueue_script('gsap-scroll-trigger', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js', ['swiper', 'gsap'], "3.12.5", true);
   wp_enqueue_script('splitType', 'https://unpkg.com/split-type', [], "0.3.4", false);
-  wp_enqueue_script('scrollable', 'https://unpkg.com/scroll-hint@latest/js/scroll-hint.min.js', ['swiper', 'gsap', 'gsap-scroll-trigger', 'splitType'], "1.0.0", true);
-  wp_enqueue_script('script-main', get_theme_file_uri('/assets/js/script.js'), ['swiper', 'gsap', 'gsap-scroll-trigger', 'splitType', 'scrollable'], '1.0.0', true);
+  wp_enqueue_script('scrollable', 'https://unpkg.com/scroll-hint@latest/js/scroll-hint.min.js', [], "1.0.0", true);
+  wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', "", "11.0", true);
+  wp_enqueue_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js', [], "3.12.5", true);
+  wp_enqueue_script('gsap-scroll-trigger', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js', ['gsap'], "3.12.5", true);
+  wp_enqueue_script('script-main', get_theme_file_uri('/assets/js/script.js'), [], '1.0.0', true);
 }
