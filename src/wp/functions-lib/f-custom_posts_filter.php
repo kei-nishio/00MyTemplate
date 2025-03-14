3458abab6bf5f103f16189e1dd3a2d46b3307c52
kei-nishio <kei.nishio.0311107@gmail.com>
2025-03-14 22:02:43 +0900

    カスタム投稿タイプの配列を追加し、投稿者でのクエリフィルターを改善

--- src/wp/functions-lib/f-custom_posts_filter.php
+++ src/wp/functions-lib/f-custom_posts_filter.php
@@ -2,7 +2,8 @@
 // ! カスタム投稿一覧を投稿者でクエリでフィルターする
 function filter_author_id_query($query)
 {
-  if (is_post_type_archive('event') && $query->is_main_query() && isset($_GET['author_id'])):
+  $post_types = array('builder', 'event', 'case');
+  if (is_main_query() && isset($_GET['author_id']) && is_post_type_archive($post_types)):
     $query->set('author', sanitize_text_field($_GET['author_id']));
   endif;
 }
