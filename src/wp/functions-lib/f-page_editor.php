b449e3bf33b6c16c6f94be9750a5f2fd6490ddf3
kei-nishio <kei.nishio.0311107@gmail.com>
2025-03-14 22:02:53 +0900

    固定ページのブロックエディタ使用条件を変更し、カスタム投稿タイプのサポートを削除

--- src/wp/functions-lib/f-page_editor.php
+++ src/wp/functions-lib/f-page_editor.php
@@ -3,7 +3,7 @@
 // 固定ページの場合
 add_filter('use_block_editor_for_post', function ($use_block_editor, $post) {
   if ($post->post_type === 'page') {
-    if (in_array($post->post_name, ['flow', 'news'])) {
+    if (in_array($post->post_name, ['top', 'home'])) {
       remove_post_type_support('page', 'editor');
       return false;
     }
@@ -11,10 +11,12 @@ add_filter('use_block_editor_for_post', function ($use_block_editor, $post) {
   return $use_block_editor;
 }, 10, 2);
 
-
 // カスタム投稿の場合
 add_action('init', 'my_remove_post_support');
 function my_remove_post_support()
 {
-  remove_post_type_support('case-study', 'editor');
+  $post_types = ['builder', 'event'];
+  foreach ($post_types as $post_type) {
+    remove_post_type_support($post_type, 'editor');
+  }
 }
