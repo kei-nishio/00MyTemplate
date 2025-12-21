24201992ddf51a14c54e363ba6e51f999f0bf04d
kei.nisho <kei.nishio@plutoid.jp>
2025-12-22 00:23:52 +0900

    delete(breadcrumb): 不要なaioseo_breadcrumbsショートコードを削除
    Breadcrumb表示の一貫性を保つため、不要なショートコードを削除した。

--- src/wp/parts/c-breadcrumb.php
+++ src/wp/parts/c-breadcrumb.php
@@ -1,17 +1,11 @@
 <div class="c-breadcrumb" vocab="http://schema.org/" typeof="BreadcrumbList">
   <div class="c-breadcrumb__inner l-inner">
     <div class="c-breadcrumb__container">
-      <?php if (true): ?>
-        <?php // All in One SEO を使う場合 
-        ?>
-        <?php echo do_shortcode('[aioseo_breadcrumbs]'); ?>
-      <?php else: ?>
-        <?php // Breadcrumb NavXT を使う場合 
-        ?>
-        <?php if (function_exists('bcn_display')) { ?>
-          <?php if (function_exists('bcn_display'))  bcn_display(); ?>
-        <?php } ?>
-      <?php endif; ?>
+      <?php // Breadcrumb NavXT を使う場合 
+      ?>
+      <?php if (function_exists('bcn_display')) { ?>
+        <?php if (function_exists('bcn_display'))  bcn_display(); ?>
+      <?php } ?>
     </div>
   </div>
 </div>
